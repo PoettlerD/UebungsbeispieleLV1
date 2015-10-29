9c9be035e98dc23856cd39bbfc6ac3d930ab6322
PoettlerD <david.poettler@gmx.at>
2015-10-29 22:19:45 +0100

    Recheck

---
+++ src/RechteckDemo.java
@@ -0,0 +1,18 @@
+
+public class RechteckDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Länge = 5;
+		int Breite = 7;
+		
+		int Umfang = 2*Länge+2*Breite;
+		int Fläche = Länge*Breite;
+		
+		System.out.println("Der Umfang beträgt: "+Umfang+"cm");
+		System.out.println("Die Fläche beträgt: "+Fläche+"cm²");
+		
+	}
+
+}
