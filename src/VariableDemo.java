4c57c1317b8eaf002597fc9cd528c3f5328966ba
PoettlerD <david.poettler@gmx.at>
2015-10-29 22:13:15 +0100

    f

---
+++ src/VariableDemo.java
@@ -0,0 +1,12 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String Vorname = "David";
+		String Nachname = "Poettler";
+		System.out.println("Der Name lautet: " + Vorname + " " + Nachname);
+	}
+
+}
